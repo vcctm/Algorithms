4a12c364a8e2484160bb9abc59aa5f7c2f06154f
Victor Miranda <victormiranda13v@gmail.com>
2022-10-20 09:27:08 -0300

    feat: add index

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 /**
-  Create an stack for handle all the brackets in the string
-  create a map to store the possible brackets
-  iterate over the string
-  if the current char is an open bracket, push it to the stack
-  if the current char is a close bracket, check if the stack is empty
-  if the stack is empty, return false
-  if the stack is not empty, pop the last element from the stack
-  check if the current char is the closing bracket of the last element
-  if it is not, return false
-  if it is, continue
-  if the stack is empty, return true
-  if the stack is not empty, return false
+  description
  */
-var lengthOfLastWord = function (s) {
-  const trimed = s.trim();
-  const trimedWords = trimed.split(' ');
-  return trimedWords[trimedWords.length - 1].length;
+var functionName = function (args) {
+  return console.log(args);
 };
 
-console.log(lengthOfLastWord('luffy is still world')); // false
+console.log(functionName('luffy is still joyboy')); // false
